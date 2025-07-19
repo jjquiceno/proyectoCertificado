c9c7fbea3c77ba955711e9675038b2805886acf8
jjquiceno <quicenolondonoj@gmail.com>
2025-07-19 11:13:36 -0500

    hola mundo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,24 +8,24 @@ import About from "./pages/About";
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Tasks from "./pages/Tasks"
-
-import { Button } from "./components/buttons";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/tasks" element={
-        <PrivateRoute>
-          <Tasks />
-        </PrivateRoute>
-      } />
-      <Route path="*" element={<h1 className="font-bold text-4xl m-50">404 Ruta no encontrada</h1>} />
-    </Routes>
+    <BrowserRouter basename="/proyectoCertificado">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/tasks" element={
+          <PrivateRoute>
+            <Tasks />
+          </PrivateRoute>
+        } />
+        <Route path="*" element={<h1 className="font-bold text-4xl m-50">404 Ruta no encontrada</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
